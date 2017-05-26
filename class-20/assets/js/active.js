e8b3efef20b13874b42a97df0535c0b0808a498c
Nazmul Hasan <nazmuls4@yahoo.com>
2017-05-26 15:01:26 +0600

    class20
    THis is class20

---
+++ class-20/assets/js/active.js
@@ -0,0 +1,25 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+		$('ul#navigation').slicknav({
+            prependTo: '.responsive-menu'
+        });
+        
+        
+    });
+
+	
+
+    jQuery(window).load(function() {
+       
+
+    });
+
+
+
+
+}(jQuery));
